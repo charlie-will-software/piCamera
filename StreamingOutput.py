2da5bdadaf2584e74c305a5a3e51a6c816d2ad8f
crs553 <charlierstubbs@pm.me>
2024-02-19 14:21:09 +0000

    Docstrings added

--- StreamingOutput.py
+++ StreamingOutput.py
@@ -4,10 +4,16 @@ from threading import Condition
 # Class to handle streaming output
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
+        """Init for camera streamer"""
         self.frame = None
         self.condition = Condition()
 
     def write(self, buf):
+        """
+        Write to streaming output
+        Inputs:
+            buf - buffer for the sreaming output
+        """
         with self.condition:
             self.frame = buf
             self.condition.notify_all()
