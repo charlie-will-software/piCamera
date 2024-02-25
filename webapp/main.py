bdbbdd6bffa716c5980ea4acda696ca5ae313140
Will Dalgleish <dev@wdalgleish.com>
2024-02-25 16:30:55 +0000

    feat: base structure for camera web app

---
+++ webapp/main.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Ctrl-C pressed! Exiting...")
+        sys.exit(0)
