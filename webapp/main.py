6df1bb76d79ea23a9de981480520e1c07e5fd76c
Will Dalgleish <dev@wdalgleish.com>
2024-02-25 17:21:52 +0000

    feat: webapp runs http server

--- webapp/main.py
+++ webapp/main.py
@@ -1,7 +1,10 @@
 import logging
+import sys
+from http.server import ThreadingHTTPServer
 
 # Local imports
 import logger
+from handler import RequestHandler
 
 
 log = logging.getLogger(__name__)
@@ -10,6 +13,13 @@ log = logging.getLogger(__name__)
 def main() -> None:
     logger.configure_initial_logger()
     log.info("---Pi Camera webapp---")
+
+    address = ("", 8000)
+    try:
+        server = ThreadingHTTPServer(address, RequestHandler)
+        server.serve_forever()
+    finally:
+        RequestHandler.camera.stop_recording()
 
 
 if __name__ == "__main__":
