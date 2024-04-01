301f42abada8568ca57a2374f54a899b835a8126
Will Dalgleish <dev@wdalgleish.com>
2024-04-01 12:41:41 +0100

    fix: format flask poc with black

--- poc/1-flask-app/app_http.py
+++ poc/1-flask-app/app_http.py
@@ -5,9 +5,11 @@ import time
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return 'Video Streaming Server'
+    return "Video Streaming Server"
+
 
 def stream():
     with PiCamera() as camera:
@@ -16,15 +18,13 @@ def stream():
         # Pre-allocate a buffer to speed up capture
         stream = io.BytesIO()
 
-        for _ in camera.capture_continuous(stream, 'jpeg',
-                                           use_video_port=True):
+        for _ in camera.capture_continuous(stream, "jpeg", use_video_port=True):
             # Reset the stream position to the beginning
             stream.seek(0)
             # Read the stream into memory as jpeg
             frame = stream.read()
             # Yield the frame in the response
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
             # Reset the stream for the next frame
             stream.seek(0)
@@ -33,10 +33,11 @@ def stream():
             # Add a small delay to reduce CPU usage
             time.sleep(0.1)
 
-@app.route('/video_feed')
+
+@app.route("/video_feed")
 def video_feed():
-    return Response(stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(stream(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
