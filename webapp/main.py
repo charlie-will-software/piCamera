982431f831f953184c80cce035f4561f8fc405c3
Will Dalgleish <dev@wdalgleish.com>
2024-04-18 21:56:49 +0100

    feat: add camera stream endpoint

--- webapp/main.py
+++ webapp/main.py
@@ -1,10 +1,27 @@
 from flask import Flask
 
+# Local imports
+from camera import Camera
+
 app = Flask(__name__)
+cam = Camera()
+cam.record()
+
 
 @app.route("/")
 def index():
-    return "Hello world!"
+
+
+def gen():
+    while True:
+        frame = cam.output.frame
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
 
 if __name__ == "__main__":
     app.run()
