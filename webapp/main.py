8297d938c0f0d53ac429d6c3620843ac1b9b6436
Will Dalgleish <dev@wdalgleish.com>
2024-05-23 21:08:11 +0100

    feat: support custom config for webapp
    also adds logging

--- webapp/main.py
+++ webapp/main.py
@@ -1,12 +1,27 @@
+import logging
 from flask import Flask, Response, render_template
 from typing import Generator
 
 # Local imports
+import logger
 from camera import Camera
+from configurator import Configuration
 
 app = Flask(__name__)
 cam = Camera()
-cam.record()
+log = logging.getLogger()
+
+
+def startup() -> None:
+    logger.configure_initial_logger()
+    config = Configuration.import_settings("configuration.yaml")
+    log.info("---Picamera webapp---")
+    log.info(f"IP address: {config.server.ip}")
+    log.info(f"Port: {config.server.port}")
+
+    cam.record()
+
+    app.run(host=config.server.ip, port=config.server.port)
 
 
 @app.route("/")
@@ -16,7 +31,7 @@ def index() -> str:
     return render_template("index.html")
 
 
-def generate_frames() -> Generator[str]:
+def generate_frames() -> Generator[str, None, None]:
     """Generates camera frames and embeds them in a string for HTTP response.
 
     Yields:
@@ -38,4 +53,4 @@ def video_feed() -> Response:
 
 
 if __name__ == "__main__":
-    app.run()
+    startup()
